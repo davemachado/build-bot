ec052c707ceca4747a6f371e86709c99e9b6fb8b
Dave Machado <dave@davemachado.com>
2017-08-03 11:36:55 -0400

    update output on successful post

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
     name='build_bot',
     packages=['build_bot'],
-    version='0.1',
+    version='1.3',
     description='A fast and easy way to manage CI builds',
     author='davemachado',
+    author_email='contact@davemachado.com',
     url='https://github.com/davemachado/build-bot',
     download_url=(
-        'https://github.com/davemachado/build-bot/tarball/0.1'),
+        'https://github.com/davemachado/build-bot/archive/1.0.tar.gz'),
     keywords=['build', 'bot', 'github'],
     classifiers=[],
-    scripts=['build_bot/build_bot.py'],
+    scripts=['build_bot/build_bot'],
 )
